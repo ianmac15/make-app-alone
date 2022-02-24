a83389975b9d3c7179713d2bc578f98775aa71c9
ianmac15 <3thita@gmail.com>
2022-02-24 21:32:05 +0200

    1

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -5,14 +5,23 @@ import { onAddInterface } from '../App'
 const AddForm = ({onAdd}:properties) => {
 
   const [newBrand, setNewBrand] = useState("")
+  const [newModel, setNewModel] = useState("")
+  const [newIsUsed, setNewIsUsed] = useState(false)
     
 
 const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
   
   e.preventDefault()
 
-
+  const newID = Math.floor(Math.random()*10000)+1
+  const newCar = {id:newID, brand:newBrand, model:newModel, isUsed: newIsUsed};
+ 
   onAdd(newCar)
+
+  setNewBrand("")
+  setNewModel("")
+  setNewIsUsed(false)
+  
 
 }
 
@@ -21,11 +30,11 @@ const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     <form className='add-form' onSubmit={onSubmit}>
       <div className='form-control'>
         <label>Car brand</label>
-        <input  type="text" placeholder='Add brand' value={newCar.brand} onChange={(e)=>setNewCar({...newCar, e.target.value})}>
+        <input  type="text" placeholder='Add brand' value={newBrand} onChange={(e)=>setNewBrand(e.target.value)}/>
       </div>
       <div className='form-control'>
         <label >Car model</label>
-        <input placeholder='Add model' type="text" />
+        <input placeholder='Add model' type="text" value={new}/>
       </div>
       <div className='form-control form-control-check'>
         <label >Car used</label>
