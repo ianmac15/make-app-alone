0ebdb28f2a7e7c508aa1650cb0c4d10db29ffff8
ianmac15 <44259297+ianmac15@users.noreply.github.com>
2022-02-25 15:12:26 +0200

    Update AddForm.tsx

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -13,6 +13,15 @@ const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
   
   e.preventDefault()
 
+  if(!newBrand) {
+    alert("Please add a brand for the car")
+    return
+  }
+  if(!newModel) {
+    alert("Please add a model for the car")
+    return
+  }
+
   const newID = Math.floor(Math.random()*10000)+1
   const newCar = {id:newID, brand:newBrand, model:newModel, isUsed: newIsUsed};
  
@@ -34,11 +43,11 @@ const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
       </div>
       <div className='form-control'>
         <label >Car model</label>
-        <input placeholder='Add model' type="text" value={new}/>
+        <input placeholder='Add model' type="text" value={newModel} onChange = {(e) => setNewModel(e.target.value)}/>
       </div>
       <div className='form-control form-control-check'>
         <label >Car used</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={newIsUsed} onChange = {(e) => setNewIsUsed(e.target.checked)}/>
       </div>
 
       <input className = 'btn btn-block' type= 'submit' value='Save Car' />
